c573207ec36b0cb08df78411db542bf55a15e46d
Lasse Hægland <seilenos@gmail.com>
2022-10-20 09:24:00 +0200

    Tesst2

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log("Getting a secret, then making a quote");
 
 const out = document.querySelector("div#quote");
 
-const token = config.env.__BEARER_TOKEN__;
+const token = process.env.__BEARER_TOKEN__;
 console.log(token);
 
 const API_BASE_URL = "https://nf-api.onrender.com/";
